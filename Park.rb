fd9319012f0e30cc49aaebd459a51e9a3e120b9c
SylvrG <shubhamgupte01@gmail.com>
2016-09-24 10:57:25 -0400

    got location texting to work

--- Park.rb
+++ Park.rb
@@ -1,13 +1,25 @@
 
 require 'twilio-ruby'
-require 'sinatra'
+require 'sinatra'  #AIzaSyBJi0e7HcPXnom-skb5b_Sy7KVfrLEiYgY 
+
+require 'net/http'
+require 'json'
+ 
+
 
 post '/sms' do
     incoming = params[:Body]
-	p incoming.reverse
+    incoming = incoming.gsub(/\s+/, "+")
+    #"600+Amphitheatre+Parkway,+Mountain+View,+CA"
+
+	p incoming
+	url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{incoming}&key=AIzaSyBJi0e7HcPXnom-skb5b_Sy7KVfrLEiYgY"
+	uri = URI(url)
+	response = Net::HTTP.get(uri)
+	response1 = JSON.parse(response)
 	
 	twiml = Twilio::TwiML::Response.new do |r|
-		r.Message(incoming)
+		r.Message(response1.to_s)
 	end
 	content_type 'text/xml'
 	twiml.text
