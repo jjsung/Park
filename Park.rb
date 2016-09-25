40e4b2826b84541627e7cd9955a0a8bd2e1d6a5e
SylvrG <shubhamgupte01@gmail.com>
2016-09-25 05:43:44 -0400

    catches some errors

--- Park.rb
+++ Park.rb
@@ -8,36 +8,69 @@ require 'json'
 
 
 post '/sms' do
+
     incoming = params[:Body]
     incoming = incoming.gsub(/\s+/, "+")
 	p incoming
+	
+	if incoming.to_s.eql?("park")
+		twiml = Twilio::TwiML::Response.new do |r|
+			r.Message("Format: Address, City, State")
+			puts "hi"
+		end
+		content_type 'text/xml'
+		twiml.text
+	
+	else
 
-	url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{incoming}&key=AIzaSyBJi0e7HcPXnom-skb5b_Sy7KVfrLEiYgY"
-	uri = URI(url)
-	response = Net::HTTP.get(uri)
-	response = JSON.parse(response)
-	response1 = response.to_s
-	lat = response1[response1.index('lat')..response1.index('lat')+16]
-	lat = lat[6..14]
-	lng = response1[response1.index('lng')..response1.index('lng')+16]
-	lng = lng[6..14]
+		# begin 
+		# 	url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{incoming}&key=AIzaSyBJi0e7HcPXnom-skb5b_Sy7KVfrLEiYgY"
+		# rescue
+		# 	twiml = Twilio::TwiML::Response.new do |r|
+		# 	r.Message("We could not find a parking space, or you formatted the address incorrectly.")
+		# 	r.Body("Format: Address, City, State")
+		# 	end
+		# end
 
-	url= "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{lat},#{lng}+&radius=50000&type=parking&key=AIzaSyBJi0e7HcPXnom-skb5b_Sy7KVfrLEiYgY"
-	uri = URI(url)
-	response2 = Net::HTTP.get(uri)
-	response2 = JSON.parse(response2)
-	response2 = response2.to_s
-	lat2 = response2[response2.index('lat')..response2.index('lat')+16]
-	lat2 = lat2[6..14]
-	lng2 = response2[response2.index('lng')..response2.index('lng')+16]
-	lng2 = lng2[6..14]
+		url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{incoming}&key=AIzaSyBJi0e7HcPXnom-skb5b_Sy7KVfrLEiYgY"
+		uri = URI(url)
+		begin
+			response = Net::HTTP.get(uri)
+			response = JSON.parse(response)
+			response1 = response.to_s
+			lat = response1[response1.index('lat')..response1.index('lat')+16]
+			lat = lat[6..14]
+			lng = response1[response1.index('lng')..response1.index('lng')+16]
+			lng = lng[6..14]
 
-	googleUrl = "http://maps.google.com/?q=loc:#{lat2},#{lng2}"
+			url= "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{lat},#{lng}+&radius=1000&type=parking&key=AIzaSyBJi0e7HcPXnom-skb5b_Sy7KVfrLEiYgY"
+			uri = URI(url)
+			response2 = Net::HTTP.get(uri)
+			response2 = JSON.parse(response2)
+			response2 = response2.to_s
+			lat2 = response2[response2.index('lat')..response2.index('lat')+16]
+			lat2 = lat2[6..14]
+			lng2 = response2[response2.index('lng')..response2.index('lng')+16]
+			lng2 = lng2[6..14]
 
-	twiml = Twilio::TwiML::Response.new do |r|
-		r.Message(googleUrl)
-			
+			googleUrl = "http://maps.google.com/?q=loc:#{lat2},#{lng2}"
+
+			twiml = Twilio::TwiML::Response.new do |r|
+				r.Message(googleUrl)
+					
+			end
+			content_type 'text/xml'
+			twiml.text
+
+		rescue Errno::ECONNREFUSED, NoMethodError
+			twiml = Twilio::TwiML::Response.new do |r|
+			r.Message("Error. Please send: Address, City, State")
+			end
+			content_type 'text/xml'
+			twiml.text
+		end
+
+		
+		
 	end
-	content_type 'text/xml'
-	twiml.text
 end
