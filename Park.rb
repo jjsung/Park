86c46ff62177a46636ba1182c09f1b92c406e5c7
SylvrG <shubhamgupte01@gmail.com>
2016-09-24 21:32:19 -0400

    working

--- Park.rb
+++ Park.rb
@@ -20,20 +20,20 @@ post '/sms' do
 	#response1 = response['results']['formatted_address']
 	response1 = response.to_s
 	lat = response1[response1.index('lat')..response1.index('lat')+16]
-	lat = lat[6..15]
-	lat= lat.to_s
+	lat = lat[6..14]
 	lng = response1[response1.index('lng')..response1.index('lng')+16]
-	lng = lng[6..15]
-	lng =lng.to_s
+	lng = lng[6..14]
 	#response3 = lat+" "+long
 
-	
-	
+	url= "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+lat.to_s+","+lng.to_s"+&radius=500&type=parking&key=AIzaSyBJi0e7HcPXnom-skb5b_Sy7KVfrLEiYgY"
+	uri = URI(url)
+	response2 = Net::HTTP.get(uri)
+	response2 = JSON.parse(response2\)
+	response2 = response2.to_s
 
 	twiml = Twilio::TwiML::Response.new do |r|
-		r.Message(lat)
-		r.Body(lat)
-		r.Body(lng)
+		r.Message(response2)
+		
 	end
 	content_type 'text/xml'
 	twiml.text
